19d8e720edfe9befd953fa2759e6775bf6dfd8fe
TorinF <32659879+TorinF@users.noreply.github.com>
2019-04-24 08:53:36 -0700

    Update Window.java

--- src/Window.java
+++ src/Window.java
@@ -1,17 +1,19 @@
+
+
 import javax.swing.*;
 public class Window
 {
     public static void main()
     {
         JFrame f=new JFrame();
+        String [] items = {"MandleBrot","other"};
+        JComboBox b=new JComboBox(items);//creating instance of JButton  
+        b.setBounds(10, 10,100, 40);//x axis, y axis, width, height  
         
-        JButton b=new JButton("click");//creating instance of JButton  
-        b.setBounds(130,100,100, 40);//x axis, y axis, width, height  
-                  
-        f.add(b);//adding button in JFrame  
-                  
+        f.add(b);//adding button in JFrame
         f.setSize(400,500);//400 width and 500 height  
         f.setLayout(null);//using no layout managers  
         f.setVisible(true);//making the frame visible  
-    } 
+    }
+    
 }
