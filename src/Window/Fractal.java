10016605ee6438c838893a576587a7edc112bedc
TorinF <32659879+TorinF@users.noreply.github.com>
2019-04-28 15:33:39 -0700

    began on fractal component

---
+++ src/Window/Fractal.java
@@ -0,0 +1,32 @@
+import java.awt.image.BufferedImage;
+import java.awt.Color;
+
+public class Fractal extends BufferedImage
+{
+    public Fractal(int width, int height, TYPE_BYTE_INDEXED)
+    {
+        super( width,  height,  );
+        drawFractal();
+    }
+    
+    /**
+     * This draws a fractal using setRGB(int x,int y,int rgb)
+     * 
+     */
+    private void drawFractal()
+    {
+        
+    }
+    
+    public static int rgb2int(int red, int green, int blue)
+    {
+        return new Color(red, green, blue).getRGB();
+        
+    }
+    
+    public static void main()
+    {
+        Fractal test = new Fractal(600,400);
+        System.out.println(test.getColorModel());
+    }
+}
