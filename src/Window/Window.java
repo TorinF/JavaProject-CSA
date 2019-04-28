8896a028663f92aea8e6778210c47a1559f10d50
TorinF <tfaes@sweetwaterschools.net>
2019-04-27 21:14:30 -0700

    Trying layout

---
+++ src/Window/Window.java
@@ -0,0 +1,29 @@
+package Window;
+
+
+
+import java.awt.FlowLayout;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+public class Window
+{
+    public static void main(String[] args) throws IOException
+    {
+        //BufferedImage image = ImageIO.read(new File("BigBrain.jpeg"));
+        //BackgroundPanel bgPanel = new BackgroundPanel(image, BackgroundPanel.SCALED, 0.0f, 0.0f);
+        JFrame f=new JFrame();
+        String [] items = {"MandleBrot","other"};
+        JComboBox d=new JComboBox(items);//creating instance of JButton  
+        JButton b = new JButton("Iterate");
+        
+        f.add(d);
+        f.add(b);//adding button in JFrame
+        f.setSize(400,500);//400 width and 500 height  
+        f.setLayout(new FlowLayout());//using no layout managers  
+        f.setVisible(true);//making the frame visible  
+    }
+    
+}
