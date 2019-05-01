7bda665d40a269ff194d379b8838e394f109bac3
TorinF <32659879+TorinF@users.noreply.github.com>
2019-05-01 07:29:30 -0700

    gg

--- src/Window/Fractal.java
+++ src/Window/Fractal.java
@@ -3,9 +3,9 @@ import java.awt.Color;
 
 public class Fractal extends BufferedImage
 {
-    public Fractal(int width, int height, TYPE_BYTE_INDEXED)
+    public Fractal(int width, int height)
     {
-        super( width,  height,  );
+        super( width,  height,  TYPE_BYTE_INDEXED);
         drawFractal();
     }
     
