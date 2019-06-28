7d9acb20acd1ad66cebcab32e5417fd12b158a94
TorinF <tfaes@sweetwaterschools.net>
2019-06-27 19:04:06 -0700

    Add uniterate binding

--- src/Mandlbrot/FracWindow.java
+++ src/Mandlbrot/FracWindow.java
@@ -22,39 +22,34 @@ import javax.swing.KeyStroke;
  */
 public class FracWindow extends JPanel {
 
-    
-    
-     
     private final int width, height;
     private Fractal frac;
     private int iter = Fractal.STARTITER;
-    
+
     public FracWindow(int width, int height) {
         // Make sure there's 0 whitespace between border of panel & image
         super(new FlowLayout(FlowLayout.LEADING, 0, 0));
-       
+
         this.width = width;
         this.height = height;
-        
-        this.setBackground(new Color(0,0,0,0));
+
+        this.setBackground(new Color(0, 0, 0, 0));
         this.setPreferredSize(new Dimension(width, height));
-        
-        frac = new Fractal(width,height);
-        
+
+        frac = new Fractal(width, height);
+
         // Converts buffered image to ImageIcon
         // images are displayed with labels
         JLabel l = new JLabel(new ImageIcon(frac));
-        
+
         // Adds single image to cover panel
         add(l);
-        
-        
+
         mapActions();
     }
-    
+
     //For button presses we need to use callable methods instead of keybinding 
     //actions
-    
     public void passiter() {
         frac.iterate();
         frac.drawFractal();
@@ -67,92 +62,102 @@ public class FracWindow extends JPanel {
     }
 
     /**
-     * This method creates keybindings that work as long as the window is in 
+     * This method creates keybindings that work as long as the window is in
      * focus
      */
     public void mapActions() {
         InputMap inMap = this.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
         inMap.put(KeyStroke.getKeyStroke("C"), "iterate");
         inMap.put(KeyStroke.getKeyStroke("X"), "iterate5");
+        inMap.put(KeyStroke.getKeyStroke("U"), "iterateLess");
         inMap.put(KeyStroke.getKeyStroke("Z"), "zoomOut");
         inMap.put(KeyStroke.getKeyStroke("SPACE"), "zoomIn");
-        
+
         inMap.put(KeyStroke.getKeyStroke("UP"), "up");
         inMap.put(KeyStroke.getKeyStroke("DOWN"), "down");
         inMap.put(KeyStroke.getKeyStroke("LEFT"), "left");
         inMap.put(KeyStroke.getKeyStroke("RIGHT"), "right");
-        
+
         Action iterate = new AbstractAction() {
-                public void actionPerformed(ActionEvent e) {
-                        frac.iterate();
-                        frac.drawFractal();
-                        repaint();
-                        
-                }
+            public void actionPerformed(ActionEvent e) {
+                frac.iterate();
+                frac.drawFractal();
+                repaint();
+
+            }
         };
-        Action iterate5= new AbstractAction() {
-                public void actionPerformed(ActionEvent e) {
-                        frac.iterate(5);
-                        frac.drawFractal();
-                        repaint();
-                        
-                }
+        Action iterate5 = new AbstractAction() {
+            public void actionPerformed(ActionEvent e) {
+                frac.iterate(5);
+                frac.drawFractal();
+                repaint();
+
+            }
+        };
+        Action iterateLess = new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                frac.iterate(-1);
+                frac.drawFractal();
+                repaint();
+            }
         };
         Action zoomOut = new AbstractAction() {
-                public void actionPerformed(ActionEvent e) {
-                        frac.zoomOut();
-                        frac.drawFractal();
-                        repaint();
-                        
-                }
+            public void actionPerformed(ActionEvent e) {
+                frac.zoomOut();
+                frac.drawFractal();
+                repaint();
+
+            }
         };
         Action zoomIn = new AbstractAction() {
-                public void actionPerformed(ActionEvent e) {
-                        frac.zoomIn();
-                        frac.drawFractal();
-                        repaint();
-                        
-                }
+            public void actionPerformed(ActionEvent e) {
+                frac.zoomIn();
+                frac.drawFractal();
+                repaint();
+
+            }
         };
         Action up = new AbstractAction() {
-                public void actionPerformed(ActionEvent e) {
-                        frac.moveUp();
-                        frac.drawFractal();
-                        repaint();
-                        
-                }
+            public void actionPerformed(ActionEvent e) {
+                frac.moveUp();
+                frac.drawFractal();
+                repaint();
+
+            }
         };
         Action down = new AbstractAction() {
-                public void actionPerformed(ActionEvent e) {
-                        frac.moveDown();
-                        frac.drawFractal();
-                        repaint();
-                        
-                }
+            public void actionPerformed(ActionEvent e) {
+                frac.moveDown();
+                frac.drawFractal();
+                repaint();
+
+            }
         };
         Action left = new AbstractAction() {
-                public void actionPerformed(ActionEvent e) {
-                        frac.moveLeft();
-                        frac.drawFractal();
-                        repaint();
-                        
-                }
+            public void actionPerformed(ActionEvent e) {
+                frac.moveLeft();
+                frac.drawFractal();
+                repaint();
+
+            }
         };
         Action right = new AbstractAction() {
-                public void actionPerformed(ActionEvent e) {
-                        frac.moveRight();
-                        frac.drawFractal();
-                        repaint();
-                }
+            public void actionPerformed(ActionEvent e) {
+                frac.moveRight();
+                frac.drawFractal();
+                repaint();
+            }
         };
-        
+
         //Creates Action map that calls actions based on input map strings
         ActionMap actmap = this.getActionMap();
         actmap.put("iterate", iterate);
         actmap.put("iterate5", iterate5);
+        actmap.put("iterateLess", iterateLess);
         actmap.put("zoomIn", zoomIn);
         actmap.put("zoomOut", zoomOut);
-        
+
         actmap.put("up", up);
         actmap.put("down", down);
         actmap.put("left", left);
